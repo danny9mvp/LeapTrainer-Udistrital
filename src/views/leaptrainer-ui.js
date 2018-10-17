1b5d222181271bc6ebae3562d9565f8b840e3695
danny9mvp <danny9mvp@gmail.com>
2018-10-17 15:39:31 -0500

    Añadido nuevo render BoneHand.

--- src/views/leaptrainer-ui.js
+++ src/views/leaptrainer-ui.js
@@ -80,6 +80,7 @@ jQuery(document).ready(function ($) {
 		existingGestureList = $("#existing-gestures"),
 		newGestureName		= $('#new-gesture-name'),
 		renderArea 			= $('#render-area'),
+		frameRiggedHand		= $('#frameRiggedHand'),
 		main				= $('#main'),
 		overlayArea			= $('#overlay'),
 		overlayShade		= $('#overlay-shade'),
@@ -134,7 +135,7 @@ jQuery(document).ready(function ($) {
 		 * When a gesture is being rendered, not all recorded frames will necessarily be needed.  This variable controls the interval between 
 		 * frames chosen for rendering.  If 21 frames are recorded and the gestureRenderInterval is 3, then just 7 frames will be rendered.
 		 */
-		gestureRenderInterval = webGl ? 3 : 6,
+		//gestureRenderInterval = webGl ? 3 : 6,
 		
 		/*
 		 * And finally we declare some working variables for use below
@@ -240,8 +241,7 @@ jQuery(document).ready(function ($) {
      */
     voiceGenre.append('<option value="' + 'Male' + '">'+'Masculino'+'</option>');
     voiceGenre.append('<option value="' + 'Female' + '">'+'Femenino'+'</option>');
-    voiceGenre.change(function(){
-    	console.log(voiceGenre.val());
+    voiceGenre.change(function(){    
     	//responsiveVoice.speak(gestureName,"Spanish Latin American "+voiceGenre.val());
     });
     for (var implName in LeapTrainer) {
@@ -440,6 +440,9 @@ jQuery(document).ready(function ($) {
 		var renderHeight 	= windowHeight - 5;
 
 		renderArea.css({width: windowWidth, height: renderHeight});
+
+		//frameRiggedHand.height(windowHeight);
+		//frameRiggedHand.width(windowWidth);
 
 		renderer.setSize(windowWidth, renderHeight);
 
@@ -749,7 +752,7 @@ jQuery(document).ready(function ($) {
 
 		var trainingGestureCount = trainer.trainingGestures;
 		
-		setOutputText('Replique el gesto o pose' + gestureName + ' ' + (trainingGestureCount > 1 ? trainingGestureCount + ' veces' : ''));
+		setOutputText('Replique el gesto o pose \'' + gestureName + '\' ' + (trainingGestureCount > 1 ? trainingGestureCount + ' veces' : ''));
 
 		gestureEntries[gestureName].css({background: 'transparent'});		
 		
@@ -764,9 +767,7 @@ jQuery(document).ready(function ($) {
 	 */
 	trainer.on('training-gesture-saved', function(gestureName, trainingSet) {
 
-		var trainingGestures = trainer.trainingGestures;
-
-		renderGesture();
+		var trainingGestures = trainer.trainingGestures;		
 		
 		var remaining = (trainingGestures - trainingSet.length);
 
@@ -781,9 +782,7 @@ jQuery(document).ready(function ($) {
 	 */
 	trainer.on('training-complete', function(gestureName, trainingSet, isPose) {
 
-		training = false;		
-		
-		renderGesture();
+		training = false;						
 
 		setOutputText(gestureName + (isPose ? ' pose aprendida!' : ' gesto aprendido!'));
 
@@ -830,9 +829,7 @@ jQuery(document).ready(function ($) {
 
 		unselectAllGestures(false);
 
-		setAllGestureScales(allHits, gestureName);
-
-		renderGesture();
+		setAllGestureScales(allHits, gestureName);		
 		
 		var hitPercentage = Math.min(parseInt(100 * hit), 100);
 
@@ -856,7 +853,6 @@ jQuery(document).ready(function ($) {
 		
 		setAllGestureScales(allHits);
 		
-		clearGesture();
 	});
 
 	/*
@@ -890,338 +886,7 @@ jQuery(document).ready(function ($) {
 	controller.on('deviceConnected', function() { enableUI('¡Conexión restablecida!'); });
 
 
-	/*
-	 * ------------------------------------------------------------------------------------------
-	 *  9. WebGL rendering functions
-	 * ------------------------------------------------------------------------------------------
-	 */
-	 /*
-	 * The camera is set to its initial position
-	 */
-	camera.position.set(cameraInitialPos.x, cameraInitialPos.y, cameraInitialPos.z);
-
-	/*
-	 * The renderer is added to the rendering area in the DOM.  The size of the renderer will be modified when the window is resized.
-	 */
-	renderArea.append(renderer.domElement);
-
-	/*
-	 * Creates a palm mesh
-	 */
-	function createPalm() { return new THREE.Mesh(palmGeometry, material); }
 	
-	/*
-	 * Creates a finger mesh
-	 */
-	function createFinger() { return new THREE.Mesh(fingerGeometry, material); }
-	
-	/*
-	 * An inital pair of palm meshs and ten fingers are added to the scene. The second palm and second five fingers 
-	 * are initially invisible.  The first palm and fingers are set in a default pose below.
-	 */
-	var palms = [createPalm(), createPalm()];
-
-	palms[1].visible = false;
-
-	scene.add(palms[0]);
-	scene.add(palms[1]);
-
-	var finger, fingers = [];
-	
-	for (var j = 0; j < 10; j++) { 
-		
-		finger = new THREE.Mesh(fingerGeometry, material);
-
-		finger.visible = j < 5;
-		
-		scene.add(finger);
-
-		fingers.push(finger); // Finger meshes are stored for animation below		
-	}
-	
-	/*
-	 * We set default a default pose for the one visible (right) hand
-	 */
-	var defaultHandPosition = true; // This is a flag used to indicate if the scene is currently just showing the default pose
-	
-	palms[0].position.set(-5.62994, -37.67400000000001, 96.368);
-	palms[0].rotation.set(-2.0921488149553125, 0.051271951412566935, -2.6597446090413466);
-
-	fingers[0].position.set(34.179, 24.22, 28.7022);
-	fingers[0].rotation.set(-2.777879785829599, 0.02183472660404244, 3.133282166633954);
-	fingers[0].scale.z = 8;
-	
-	fingers[1].position.set(53.8033, -15.913000000000011, 32.6661);
-	fingers[1].rotation.set(-2.7753644328170965, 0.22532594370921782, 3.056111568660471);
-	fingers[1].scale.z = 5;
-	
-	fingers[2].position.set(4.69965, 49.19499999999999, 31.643);
-	fingers[2].rotation.set(-2.600622653205929, 0.033504548426940645, 3.121471314695975);
-	fingers[2].scale.z = 9;
-	
-	fingers[3].position.set(-23.7075, 50.976, 50.363);
-	fingers[3].rotation.set(-2.543443897235925, 0.04106473211751575, 3.113625377842598);
-	fingers[3].scale.z = 8;
-	
-	fingers[4].position.set(-80.6532, -33.772999999999996, 84.7031);
-	fingers[4].rotation.set(-2.589002343898949, -0.4631619960981157, -2.872745378807403);
-	fingers[4].scale.z = 6;
-
-	/*
-	 * Updates the material of the palm and fingers created above.  This function is called when recording starts and ends, in order to 
-	 * modify how visible hands look during recording.
-	 */
-	function setHandMaterial(m) {
-		
-		palms[0].material = m;
-		palms[1].material = m;
-		
-		for (var i = 0, l = fingers.length; i < l; i++) { fingers[i].material = m; }		
-	}
-	
-	/*
-	 * We set the recording material during recording.
-	 */
-	trainer.on('started-recording', function () { setHandMaterial(recordingMaterial); })
-		   .on('stopped-recording', function () { setHandMaterial(material); });
-	
-	/*
-	 * We use Paul Irish's requestAnimFrame function (which is described 
-	 * here: http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/) for 
-	 * updating the scene.
-	 * 	
-	 */
-	window.requestAnimFrame = (function(){
-		  return  window.requestAnimationFrame       ||
-		          window.webkitRequestAnimationFrame ||
-		          window.mozRequestAnimationFrame    ||
-		          function(callback){ window.setTimeout(callback, 1000 / 60); };
-		})();
-	
-	/*
-	 * And bind a simple update function into the requestAnimFrame function
-	 */
-	function updateRender() { controls.update(); TWEEN.update(); renderer.render(scene, camera); requestAnimFrame(updateRender); }
-
-	requestAnimFrame(updateRender);
-
-	/*
-	 * In order to avoid as much variable creation as possible during animation, variables are created here once.
-	 */
-	var hand, palm, position, direction, normal, handFingers, handFingerCount, finger, handCount, palmCount = palms.length;
-
-	/*
-	 * TODO: WHY is it necessary to offset mesh positions on the y-axis by 170? I don't know at the moment - but this bit of nonsense should be fixed.
-	 */
-	var yOffset = -170;	
-	
-	/*
-	 * This section animates the fingers and palm. 
-	 * 
-	 * The positionPalm and positionFinger functions, as well as the structure of the leap controller listener below, are based on code 
-	 * from jestPlay (also under the MIT license), by Theo Armour:
-	 * 
-	 * 	http://jaanga.github.io/gestification/cookbook/jest-play/r1/jest-play.html
-	 * 
-	 * Thanks Theo!
-	 */
-	
-	/**
-	 * 
-	 */
-	function positionPalm(hand, palm) {
-		
-		position = hand.stabilizedPalmPosition || hand.position;
-
-		palm.position.set(position[0], position[1] + yOffset, position[2]); 
-
-		direction = hand.direction;
-		
-		palm.lookAt(new THREE.Vector3(direction[0], direction[1], direction[2]).add(palm.position));
-
-		normal = hand.palmNormal || hand.normal;
-		
-		palm.rotation.z = Math.atan2(normal[0], normal[1]);
-	}
-	
-	/**
-	 * 
-	 */
-	function positionFinger(handFinger, finger) {
-
-		position = handFinger.stabilizedTipPosition || handFinger.position;
-		
-		finger.position.set(position[0], position[1] + yOffset, position[2]);
-
-		direction = handFinger.direction;
-		
-		finger.lookAt(new THREE.Vector3(direction[0], direction[1], direction[2]).add(finger.position));
-		
-		finger.scale.z = 0.1 * handFinger.length;		
-	}
-	
-	/*
-	 * Now we set up a Leap controller frame listener in order to animate the scene
-	 */
-	var clock = new THREE.Clock();
-	
-	clock.previousTime = 1000000;	
-
-	controller.on('frame', function(frame) {
-
-		if (clock.previousTime === 1000000) {
-
-			handCount = frame.hands.length;
-			
-			for (var i = 0; i < palmCount; i++) { // We attempt to position all (normally, both) rendered hands
-				
-				palm = palms[i];
-
-				if (i >= handCount) {
-				
-					if (!defaultHandPosition) { // If the default pose is showing we don't update anything
-
-						palm.visible = false;
-
-						for (var j = 0, k = 5, p; j < k; j++) { p = (i * 5) + j; fingers[p].visible = false; };						
-					}
-
-				} else {
-					
-					defaultHandPosition = false;
-					
-					hand = frame.hands[i];
-
-					positionPalm(hand, palm);
-					
-					palm.visible = true;
-
-					handFingers 	= hand.fingers;
-					handFingerCount = handFingers.length;
-
-					/*
-					 * 
-					 */
-					for (var j = 0, k = 5; j < k; j++) {
-						
-						finger = fingers[(i * 5) + j];
-
-						if (j >= handFingerCount) {
-							
-							finger.visible = false;
-							
-						} else {
-
-							positionFinger(handFingers[j], finger);
-							
-							finger.visible = true;
-						}
-					};
-				}
-			}	
-		}
-	});	
-	
-	/*
-	 * Finally we set up the rendering of gestures.  Gestures are rendered by placing hand renders periodically along a recorded set of 
-	 * hand positions. 
-	 * 
-	 * We save each render in the renderedHands array so that the previous gesture can be deleted before a new one is rendered.
-	 */
-	var renderedHands = [];
-	
-	/*
-	 * Removes the currently rendered gesture, if any.
-	 */
-	function clearGesture() {
-		
-		new TWEEN.Tween(camera.position).to({x: cameraInitialPos.x, y: cameraInitialPos.y, z: cameraInitialPos.z}).easing(TWEEN.Easing.Exponential.Out).start();
-		new TWEEN.Tween(camera.rotation).to({x: 0, y: 0, z: 0}).easing(TWEEN.Easing.Exponential.Out).start();
-
-		for (var i = 0, l = renderedHands.length; i < l; i++) { scene.remove(renderedHands[i]); }
-		
-		renderedHands = [];
-	}
-	
-	/**
-	 * This function is called when a training gesture is saved and when a gesture is recognized.  It depends on the LeapTrainer 
-	 * Controller providing a renderableGesture array.
-	 */
-	function renderGesture() {
-		
-		if (!webGl) { return; } // Gesture renders are entirely disabled for canvas renderers (it's just too slow at the moment!)
-
-		/*
-		 * Only one gesture is rendered at a time, so first the current gesture - if any - is removed.
-		 */
-		clearGesture();
-
-		/*
-		 * The LeapTrainer controller should provide a renderableGesture array, which should always contain positioning data for the 
-		 * LAST gesture recorded.
-		 */
-		var gestureFrames = trainer.renderableGesture;
-		
-		if (!gestureFrames || gestureFrames.length == 0) { return; } // If the controller doesn't supply this variable, or the array is empty, we return.
-
-		/*
-		 * Some variables are set up in order to avoid creation in the loops
-		 */
-		var frame, hand, handObject, palm, fingers, finger, fingerMesh, material;
-		
-		for (var i = 0, l = gestureFrames.length; i < l; i += gestureRenderInterval) { // Not all frames are necessarily rendered
-			
-			frame = gestureFrames[i];
-			
-			/*
-			 * TODO: It sucks that new materials are being created in a loop here - but the frame count is variable, and the opacity increases 
-			 * as the frame gets closer to the end.. So for the moment, this is how it is.
-			 */
-			material = new THREE.MeshBasicMaterial({wireframe: true, color: white, transparent: true, opacity: Math.min(0.02 * i, 0.5) });
-			
-			for (var j = 0, k = frame.length; j < k; j++) {
-				
-				hand = frame[j];
-				
-				handObject = new THREE.Object3D();
-				
-				/*
-				 * Palm
-				 */
-				palm = createPalm();
-				
-				palm.material = material;
-				
-				positionPalm(hand, palm);
-
-				handObject.add(palm);
-				
-				/*
-				 * Fingers
-				 */	
-				fingers = hand.fingers;
-
-				for (var p = 0, q = fingers.length; p < q; p++) {
-					
-					finger = fingers[p];
-					
-					fingerMesh = createFinger();
-
-					fingerMesh.material = material;
-					
-					positionFinger(finger, fingerMesh);
-
-					handObject.add(fingerMesh);
-				}
-
-				renderedHands.push(handObject);
-				
-				scene.add(handObject);
-			}
-		}
-	}	
-
-
 	/*
 	 * ------------------------------------------------------------------------------------------
 	 *  10. And finally...
@@ -1231,22 +896,7 @@ jQuery(document).ready(function ($) {
 	/*
 	 * And finally we connect to the device
 	 */
-	controller.connect();
-	//trainer.fromJSON('{"name":"MAMA","pose":true,"data":[[{"x":0.16151751446330626,"y":0.04247126540953283,"z":0.07556717733356622,"stroke":1},{"x":0.05498638440114911,"y":0.035775389369676946,"z":0.07472387839582928,"stroke":1},{"x":-0.051544745661008085,"y":0.02907951332982106,"z":0.07388057945809212,"stroke":1},{"x":-0.15807587572316528,"y":0.022383637289965147,"z":0.07303728052035519,"stroke":1},{"x":-0.2646070057853225,"y":0.01568776125010926,"z":0.07219398158261814,"stroke":1},{"x":-0.37113813584747973,"y":0.00899188521025357,"z":0.07135068264488109,"stroke":1},{"x":0.6288618641525203,"y":-0.15438945185935884,"z":-0.44075357993534203,"stroke":1}]]}');
-	//trainer.fromJSON('{"name":"MAMA", "pose" : true, "data" : [[{"x":0.16151751446330626,"y":0.04247126540953283,"z":0.07556717733356622,"stroke":1},{"x":0.05498638440114911,"y":0.035775389369676946,"z":0.07472387839582928,"stroke":1},{"x":-0.051544745661008085,"y":0.02907951332982106,"z":0.07388057945809212,"stroke":1},{"x":-0.15807587572316528,"y":0.022383637289965147,"z":0.07303728052035519,"stroke":1},{"x":-0.2646070057853225,"y":0.01568776125010926,"z":0.07219398158261814,"stroke":1},{"x":-0.37113813584747973,"y":0.00899188521025357,"z":0.07135068264488109,"stroke":1},{"x":0.6288618641525203,"y":-0.15438945185935884,"z":-0.44075357993534203,"stroke":1}]]}');	
-
-	//trainer.on('MAMA', function() { alert('Go left!'); });
+	controller.use('boneHand',{
+  			targetEl: document.getElementById('render-area')  						
+  		}).connect();
 });
-
-  $(document).ready(function() {
-
-
-  	var controller = new Leap.Controller();
-	var trainer = new LeapTrainer.Controller({controller: controller});
-
-  	var leapController = new Leap.Controller();
-  	var trainer = new LeapTrainer.Controller({controller: leapController});
-
-			leapController.connect();
-			
-		});
